005576773608912c6edad4b5157b01cf208393c5
peter@mobilefringe.com <peter@mobilefringe.com>
2015-04-20 16:51:06 -0400

    via kodekloud.io (5512e0606e6f64112d050000)

--- assets/application.js
+++ assets/application.js
@@ -604,6 +604,5 @@ $(document).ready(function() {
     $('.desktop_search_bar_form').on('submit', function(e) {
         e.preventDefault();
         window.location.href = "/search/"+$("#desktop_search_bar").val();
-        // $('.desktop_search_bar_form').attr('action', "/test1").submit();
     });
 });
